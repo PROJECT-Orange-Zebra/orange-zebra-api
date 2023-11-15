dc765f04f5300c165cdc344d353f4f9500ec7a29
Haarith Ahmed <Haarith.ahmed13@gmail.com>
2023-11-14 22:09:39 -0500

    part 1 catalog branch

---
+++ Orange.Zebra.Domain/Catalog/Rating.cs
@@ -0,0 +1,30 @@
+namespace Orange.Zebra.Domain.Catalog
+{
+
+public class Rating
+{
+    public int Stars {get; set;}
+    public string UserName {get; set;}
+    public string Review {get; set;}
+
+
+        public Rating(int stars, string userName, string review)
+    {
+        if(stars < 1 || stars > 5)
+        {
+        throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
+        }
+
+        if(string.IsNullOrEmpty(userName))
+        {
+            throw new ArgumentException("UserName cannot be null.");
+        }
+
+    this.Stars = stars;
+    this.UserName = userName;
+    this.Review = review;
+    }
+}
+}
+﻿
+
