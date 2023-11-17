efaca964505acd945df000d041bc6e69947dc41d
Andres San Miguel <andressm2013@gmail.com>
2023-11-16 19:06:50 -0500

    Part 2 (3) Configure the API

--- Orange.Zebra.Api/Controllers/CatalogController.cs
+++ Orange.Zebra.Api/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc; 
 using Orange.Zebra.Domain.Catalog;
+using Orange.Zebra.Data;
 
 
 namespace Orange.Zebra.Api.Controllers
@@ -8,16 +9,16 @@ namespace Orange.Zebra.Api.Controllers
 	[Route("[controller]")]
 	public class CatalogController: ControllerBase
 	{
+        private readonly StoreContext _db;
+
+        public CatalogController(StoreContext db)
+        {
+            _db = db;
+        }
         [HttpGet]
         public IActionResult GetItems()
         {
-            var items = new List<Item>()
-            {
-            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-            new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
-            };
-
-            return Ok(items);
+            return Ok(_db.Items);
         }
 
         [HttpGet("{id:int}")]
--- Orange.Zebra.Domain/Catalog/Rating.cs
+++ Orange.Zebra.Domain/Catalog/Rating.cs
@@ -3,6 +3,7 @@ namespace Orange.Zebra.Domain.Catalog
 
 public class Rating
 {
+    public int Id { get; set; }
     public int Stars {get; set;}
     public string UserName {get; set;}
     public string Review {get; set;}
