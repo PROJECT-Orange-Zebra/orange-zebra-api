bddb658351a04f76235b7bf8a059aae4bca127ee
Neel Bhanvadia <cravingsdeal@gmail.com>
2023-11-26 19:21:40 -0500

    pg 24 to 28

--- Orange.Zebra.Api/Controllers/CatalogController.cs
+++ Orange.Zebra.Api/Controllers/CatalogController.cs
@@ -15,6 +15,7 @@ namespace Orange.Zebra.Api.Controllers
         {
             _db = db;
         }
+
         [HttpGet]
         public IActionResult GetItems()
         {
@@ -24,37 +25,66 @@ namespace Orange.Zebra.Api.Controllers
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id)
         {
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id;
-
-            return Ok(item);
+            var item = _db.Items.Find(id);
+            if (item == null)
+            {
+	            return NotFound();
+            }
+            return Ok();
         }
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42",item);
+            _db.Items.Add(item);
+	        _db.SaveChanges();
+	        return Created($”/catalog/{item.Id}”, item);
         }
+
         [HttpPost("{id:int}/ratings")]
         public IActionResult PostRating(int id, [FromBody] Rating rating)
         {
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id;
-            item.AddRating(rating);
+            var item = _db.Items.Find(id);
+	        if (item == null)
+            {
+                return NotFound();
+            }
+            item.AddRating(Rating);
+            _db.SaveChanges();
 
             return Ok(item);
         }
+
         [HttpPut("{id:int}")]
-        public IActionResult Put(int id, Item item)
+        public IActionResult PutItem(int id, [FromBody] Item item)
         {
+            if (id ≠ item.Id)
+            {
+                return BadRequest();
+            }
+            if (_db.Items.Find(id) == null)
+            {
+                return NotFound();
+            }
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+
             return NoContent();
         }
+
 
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
-            return NoContent();
+            var item = _db.Items.Find(id);
+	        if (item == null)
+	        {
+		        return NotFound();
+	        }
+            _db.ItemsRemove(item);
+            _db.SaveChanges();
+            
+            return Ok();
         }
-
     }
 }
-
