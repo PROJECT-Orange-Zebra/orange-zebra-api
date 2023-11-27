f20e0b57f618d22731c6878d609f386a1faa4831
Kyle Hrabusa <hrabusa.4@osu.edu>
2023-11-26 19:52:28 -0500

    Top 31

---
+++ Orange.Zebra.Data/StoreContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Orange.Zebra.Data;
+
+public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext>
+{
+    public StoreContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+
+        optionsBuilder.UseSqlite("Data Source=../Registrar.sqlite");
+
+        return new StoreContext(optionsBuilder.Options);
+    }
+}
